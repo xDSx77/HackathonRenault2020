4a21c6a4018e38f02852be792bbcfe1bb27a665f
Ferdinand Bhavsar <ferdinand.bhavsar@epita.fr>
2020-02-05 17:11:08 +0100

    Simple shortest path with car
    -Simple filters
    -No multi-modal paths

--- api/shortestpath/shortestpath.py
+++ api/shortestpath/shortestpath.py
@@ -3,6 +3,16 @@ sys.path.insert(1, '../rest/')
 
 from ecosystem_api import Ecosystem
 
+def get_available_cars(ecosystem: Ecosystem):
+    vehicles = []
+    cars = ecosystem.get_all_vehicles_info()
+    for car in cars:
+        if car["available"]:
+            vehicles.append({"id": car["attitude"]["id"], "x": car["attitude"]["position"]["x"], "y": car["attitude"]["position"]["y"]})
+    return vehicles
+
+
+
 def get_shortest_paths(src: dict, dst: dict, filters: list, url: str):
     ecosystem = Ecosystem(url, "env")
     dict_filters = {"walk": ecosystem.get_shortest_path_walk,
@@ -14,7 +24,9 @@ def get_shortest_paths(src: dict, dst: dict, filters: list, url: str):
     for key, function in dict_filters.items():
         if len(filters) == 0 or key in filters:
             if key == "car":
-                continue # TODO
+                available_cars = get_available_cars(ecosystem)
+                if len(available_cars) != 0:
+                    shorter_paths[key] = function(src, dst, available_cars)
             else:
                 shorter_paths[key] = function(src, dst)
     return shorter_paths
