1743ff25c3e98b54228786040a817789116b5726
Alexandre Delaunay <alexandre.delaunay@epita.fr>
2020-02-05 19:58:07 +0100

    [FRONT] update front

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -9,6 +9,8 @@ import { DomSanitizer } from '@angular/platform-browser';
 })
 export class AppComponent {
   title = 'HackathonRenault2020Front';
+  isMap = true;
+  buttonText = "Changer de trajet";
 
   constructor(
     private matIconRegistry: MatIconRegistry,
@@ -23,5 +25,13 @@ export class AppComponent {
     // Air quality icons
     this.matIconRegistry.addSvgIcon('air-normal', this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/svg/heart.svg'));
     this.matIconRegistry.addSvgIcon('air-pollution peak', this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/svg/co2.svg'));
+  }
+
+  changeView() {
+    this.isMap = !this.isMap;
+    if (this.isMap)
+      this.buttonText = "Changer de trajet";
+    else
+      this.buttonText = "Retour";
   }
 }
