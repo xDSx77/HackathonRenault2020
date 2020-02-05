be534ce63d4fbd4fe403dbf6a08cfd754397f6bb
Alexandre Delaunay <alexandre.delaunay@epita.fr>
2020-02-05 18:01:04 +0100

    [FRONT] add expantion panel

---
+++ app/src/app/ways/ways.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+
+@Component({
+  selector: 'app-ways',
+  templateUrl: './ways.component.html',
+  styleUrls: ['./ways.component.scss']
+})
+export class WaysComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
