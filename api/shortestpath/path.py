d14908e1a89345d2dfd5f26a7965836e390995a9
Bruno <bruno.zheng@outlook.fr>
2020-02-05 18:01:27 +0100

    ADD new class path and update function to return a dict of path

---
+++ api/shortestpath/path.py
@@ -0,0 +1,18 @@
+class Path:
+    """
+    Path of source to destination
+    """
+
+    src = None
+    dst  = None
+    list_sub_path = None
+    total_cost = None
+
+
+    def __init__(self, src, dst, list_sub_path, total_cost):
+        self.src = src
+        self.dst = dst
+        self.list_sub_path = list_sub_path
+        self.total_cost = total_cost
+
+
--- api/shortestpath/shortestpath.py
+++ api/shortestpath/shortestpath.py
@@ -2,6 +2,7 @@ import sys
 sys.path.insert(1, '../rest/')
 
 from ecosystem_api import Ecosystem
+from path import Path
 
 def get_available_cars(ecosystem: Ecosystem):
     vehicles = []
@@ -12,21 +13,73 @@ def get_available_cars(ecosystem: Ecosystem):
     return vehicles
 
 
+def get_subway_path(eco, src: dict, dst: dict) -> list:
+
+    # Get path between src to dst
+    subway_path = eco.get_shortest_path_metro(src, dst)
+    if subway_path == {}:
+        return []
+
+    list_sub_path = []
+    last_path = None
+
+    # If first station not in src -> add a path of walk to first station
+    first_station = subway_path['paths'][1]
+    if src['x'] != second_path[0] or src['y'] == second_path[1]:
+
+        walk_path = eco.get_shortest_paths_walk(src, second_path)
+        if walk_path == {}:
+            return []
+
+        list_sub_path.append(walk)
+
+    # If last station not in dst -> add a path of walk to dst
+    last_station = subway_path['paths'][-1]
+    if dst['x'] != last_station[0] or dst['y'] != last_station[1]:
+        last_path = exo.get_shortest_paths({'x': last_station[0], 'y': last_station[1]}, dst)
+
+
+    subway_path['paths'] = subway_path['paths'][1:]
+    subway_path['cost'] = subway_path['cost'][1:]
+    list_sub_path.append(subway_path)
+
+
+    if last_path:
+        list_sub_path.append(last_path)
+
+
+    total_cost = 0
+    for elt in list_sub_path:
+        total_cost += float(elt['path_lenght'])
+
+    return Path(src, dst, list_sub_path, total_cost)
+
 
 def get_shortest_paths(src: dict, dst: dict, filters: list, url: str):
     ecosystem = Ecosystem(url, "env")
     dict_filters = {"walk": ecosystem.get_shortest_path_walk,
-                    "metro": ecosystem.get_shortest_path_metro,
+                    "metro": get_subway_path,
                     "bike": ecosystem.get_shortest_path_bike,
                     "car": ecosystem.get_shortest_path_car}
 
     shorter_paths = {}
     for key, function in dict_filters.items():
+
+        print('KEY:', key)
+
         if len(filters) == 0 or key in filters:
             if key == "car":
                 available_cars = get_available_cars(ecosystem)
                 if len(available_cars) != 0:
                     shorter_paths[key] = function(src, dst, available_cars)
+            elif key == "metro":
+                shorter_paths[key] = function(ecosystem, src, dst)
             else:
-                shorter_paths[key] = function(src, dst)
+
+                tmp_dic_path = function(src, dst)
+                total_cost = float(tmp_dic_path['cars']['path_lenght'])
+                tmp_path = Path(src, dst, [tmp_dic_path], total_cost)
+
+                shorter_paths[key] = tmp_path
+
     return shorter_paths
