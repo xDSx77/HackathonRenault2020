7ee75672980aa993e50d8229c172cf8c26d8f558
Nicolas Nys <nicolas.nys@epita.fr>
2020-02-06 01:44:08 +0100

    add shell for Game Master increase the interraction with the Game

--- api/mqtt/GameMaster.py
+++ api/mqtt/GameMaster.py
@@ -4,8 +4,30 @@ from teleport_agent import teleport_agent
 from weather import change_weather
 from polution import change_polution
 from road import *
+from subway_line import subway_lines
 
 from time import sleep
+
+def close_some_road(close : str):
+    car_a = Road("edge_54", close)
+    car_b = Road("edge_56", close)
+    list_car = [car_a,car_b]
+    bike_a = Road("edge_12", close)
+    bike_b = Road("edge_14", close)
+    list_bike = [bike_a,bike_b]
+    walk_a = Road("edge_32", close)
+    walk_b = Road("edge_34", close)
+    list_walk = [walk_a, walk_b]
+    change_road(list_car, list_bike, list_walk)
+
+def close_some_subway(close : str):
+    list_line = ["edge_6","edge_12"]
+    list_status = [close, close]
+    subway_lines(list_line, list_status)
+
+
+def reset():
+    reset_agent()
 
 def snow():
     change_weather("snow")
@@ -19,7 +41,7 @@ def heat():
 def polution_normal():
     change_polution("normal")
 
-def polution_normal():
+def polution_eleve():
     change_polution("pollution peak")
 
 
@@ -34,24 +56,38 @@ def little_smow_storm():
     sleep(7)
     change_weather("normal")
 
+def hard_test1():
+    rain()
+    close_some_road("close")
+    close_some_subway("close")
 
-def close_some_road():
+def hard_test2():
+    polution_eleve()
+    close_some_road()
+    close_some_subway()
+
+def hard_test3():
+    rain()
+    close_some_road("close")
+    close_some_subway("close")
+    sleep(3)
+    change_weather("normal")
+    sleep(2)
+    close_some_road("open")
+    close_some_subway("open")
+
+def hard_test4():
+    start_aeroport()
+    polution_eleve()
+    #fermer la route de l'aeroport.
+    rain()
 
 
-    car_a = Road("edge_54", "close")
-    car_b = Road("edge_56", "close")
-    list_car = [car_a,car_b]
-
-    bike_a = Road("edge_12", "close")
-    bike_b = Road("edge_14", "close")
-    list_bike = [bike_a,bike_b]
 
 
-    walk_a = Road("edge_32", "close")
-    walk_b = Road("edge_34", "close")
-    list_walk = [walk_a, walk_b]
 
-    change_road(list_car, list_bike, list_walk)
+
+
 
 
 
