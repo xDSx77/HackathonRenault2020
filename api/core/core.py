ed2f93535595fc413ff1198c9d8dad8cc571587b
Ferdinand Bhavsar <ferdinand.bhavsar@epita.fr>
2020-02-06 10:30:07 +0100

    Modifiedd core

--- api/core/core.py
+++ api/core/core.py
@@ -1,7 +1,10 @@
 import sys
 import os
-from flask import Flask
 import json
+
+from flask import Flask, request
+
+app = Flask(__name__)
 
 sys.path.insert(1, "../shortestpath")
 import shortestpath
@@ -9,27 +12,14 @@ import shortestpath
 config_path = "../config.dev.json"
 
 
-def set_config_path() -> bool:
-    """
-    Set config file path, return true if valid, false otherwise
-    :param path:
-    :return:
-    """
-    path = "" # TODO request
-    if not os.path.isfile(path):
-        return False
-
-    config_path = path
-    return True
-
-
+@app.route("/api/get/shortestpath", methods=['GET', 'POST'])
 def get_shortest_paths() -> dict:
     """
     Get shortest path according to config file
     """
-    filters = "" # TODO request text
-    src = {"x": 0, "y": 0} # TODO request text
-    dst = {"x": 0, "y": 0} # TODO request text
+    filters = []  # TODO get filters
+    src = {"x": 0, "y": 0} # TODO get dst and src
+    dst = {"x": 0, "y": 0}
     with open(config_path) as fp:
         data = json.load(fp)
         if "url" in data:
