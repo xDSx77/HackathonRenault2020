c7086e02a2bed59bd86cda8854baeb263be69d1d
Bryan Andriamasy <bryan.andriamasy@epita.fr>
2020-02-06 10:56:04 +0100

    [FRONT] home -> ways

--- app/src/app/mission-dialog/mission-dialog.component.ts
+++ app/src/app/mission-dialog/mission-dialog.component.ts
@@ -1,7 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MissionData } from '../mission-data';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-mission-dialog',
   templateUrl: './mission-dialog.component.html',
@@ -11,10 +11,12 @@ export class MissionDialogComponent {
 
   constructor(
     public dialogRef: MatDialogRef<MissionDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: MissionData
+    @Inject(MAT_DIALOG_DATA) public data: MissionData,
+    private router: Router
   ) { }
 
   onClick(): void {
     this.dialogRef.close();
+    this.router.navigate(['ways']);
   }
 }
