d659da3b2089d22a1f81e78ad2a5699270079da0
Antoine Dray <antoine.dray@epita.fr>
2020-02-06 15:37:42 +0100

    Update init file

--- init.sh
+++ init.sh
@@ -6,10 +6,26 @@ set -e
 # Install all python packages
 if [ -x "$(command -v pip3)" ]; then
     pip3 install -r api/requirements.txt --user
-else
+elif [ -x "$(command -v pip)" ]; then
     pip install -r api/requirements.txt --user
+else
+    echo "Could not find pip"
+    echo "Quitting..."
+    exit 1
 fi
 
 export FLASK_APP="api/flask_api.py"
 
 flask run
+
+if [ -x "$(command -v python3)" ]; then
+    python3 -m flask run
+elif [ -x "$(command -v python)" ]; then
+    python -m flask run
+else
+    echo "Could not find python"
+    echo "Quitting..."
+    exit 1
+fi
+
+exit 0
