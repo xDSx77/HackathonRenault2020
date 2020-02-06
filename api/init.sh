435063942ef3676068e2ace50250d6e49a067119
Allan Cantin <allan.cantin@epita.fr>
2020-02-06 15:54:40 +0100

    fix: move init.sh in api/

--- api/init.sh
+++ api/init.sh
@@ -5,16 +5,16 @@ set -e
 
 # Install all python packages
 if [ -x "$(command -v pip3)" ]; then
-    pip3 install -r api/requirements.txt --user
+    pip3 install -r requirements.txt --user
 elif [ -x "$(command -v pip)" ]; then
-    pip install -r api/requirements.txt --user
+    pip install -r requirements.txt --user
 else
     echo "Could not find pip"
     echo "Quitting..."
     exit 1
 fi
 
-export FLASK_APP="api/flask_api.py"
+export FLASK_APP="flask_api.py"
 
 if [ -x "$(command -v python3)" ]; then
     python3 -m flask run
